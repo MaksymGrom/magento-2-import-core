cb7502ba6cd017b498bb11e2f17207adab4df2a7
Maksym <MailMaximGrom@gmail.com>
2018-09-08 18:30:12 +0300

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Grom Maksym <MailMaximGrom@gmail.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Grom_ImportCore',
+    __DIR__
+);
